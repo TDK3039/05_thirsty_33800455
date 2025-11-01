d7cdb2dfa894b6893792c8013811bd1a5d8e025d
Drajah223 <davidrajah325@outlook.com>
2025-11-01 14:57:36 +0000

    Initial Version

---
+++ index.js
@@ -0,0 +1,21 @@
+// Setup express and ejs
+var express = require ('express')
+var ejs = require('ejs')
+
+// Create the express application object
+const app = express()
+const port = 8000
+
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs');
+
+//Middleware before routes Body parser
+app.use(express.urlencoded({ extended: true }));
+//Serve files
+app.use(express.static("public"));
+// Load the route handlers
+const mainRoutes = require("./routes/main");  
+app.use('/', mainRoutes);
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/main.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+
+//Define Data
+var shopData = { shopName: "The Hydration Hub", 
+    productCategories: ["Alcohol", "Soft Drinks", "Hot Drinks"],
+    shopLocations: [
+        {
+            manager: "James Bond",
+            address: "Flat 1, 17 Berkeley Crescent, Mayfair, London"
+        },
+        {
+            manager: "Alex Rider",
+            address: "Unit 3, 42 Hackford Lane, Shoreditch, London"
+        }, 
+        {
+            manager: "Eve MoneyPenny",
+            address: "12 Queen Anne’s Gate, Westminster, London"
+        }
+
+
+    ]
+};
+
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs", shopData);
+});
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData);
+});
+
+router.get("/search",(req, res) => {
+    res.render("search.ejs", shopData);
+});
+
+router.get('/search_result', function (req, res){
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+});
+
+router.get("/register", (req, res) => {
+    res.render("register.ejs", shopData);
+});
+
+router.post("/registered", (req, res) => {
+    const first = req.body.first.trim();
+    const last = req.body.last.trim();
+    const email = req.body.email.trim();
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)){
+        return res.send("This is an invalid email address. Please go back and enter a valid email.")
+    }
+
+    res.send(`Hello ${first} ${last}, you are now registered with this email!: ${email}`);
+});
+
+//Survey Routes 
+router.get("/survey", (req, res) => {
+  res.render("survey.ejs", shopData);
+});
+
+router.post("/survey_result", (req, res) => {
+  const { first, last, email, age, category, student } = req.body;
+   let categories = req.body.category;
+
+  // Ensure categories is always an array
+  if (!Array.isArray(categories)) {
+    categories = categories ? [categories] : [];
+  }
+  const isStudent = student === "yes" ? "Yes" : "No";
+
+  res.render("survey_result.ejs", {
+    shopName: shopData.shopName,
+    first,
+    last,
+    email,
+    age,
+    categories,
+    isStudent
+  });
+});
+// TODO
+
+module.exports = router;
